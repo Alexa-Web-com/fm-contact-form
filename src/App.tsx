3982ebc10f8a32c46f4ac756f72b590e43051113
Aleksandra Wilczynska <aleksandra.wilczynska@publicisgroupe.com>
2024-05-24 12:44:04 +0200

    feat: add states

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [generalEnquiry, setGeneralEnquiry] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [consent, setConsent] = useState<boolean>(false);
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("submitted!");
@@ -17,10 +25,12 @@ function App() {
                 First Name<sup>*</sup>
               </label>
               <input
-                className="rounded"
+                className="rounded_border"
                 type="text"
                 id="firstName"
                 name="firstName"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
               />
             </div>
             <div className="input_container">
@@ -28,10 +38,12 @@ function App() {
                 Last Name<sup>*</sup>
               </label>
               <input
-                className="rounded"
+                className="rounded_border"
                 type="text"
                 id="lastName"
                 name="lastName"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
               />
             </div>
           </div>
@@ -40,10 +52,12 @@ function App() {
               Email Address<sup>*</sup>
             </label>
             <input
-              className="rounded"
+              className="rounded_border"
               type="email"
               id="emailAddress"
               name="emailAddress"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
           <div>
@@ -52,12 +66,26 @@ function App() {
             </p>
           </div>
           <ul className="elem_container">
-            <li className="rounded">
-              <input type="radio" id="generalEnquiry" name="queryType" />
+            <li className="rounded_border">
+              <input
+                type="radio"
+                id="generalEnquiry"
+                name="queryType"
+                value="General Enquiry"
+                checked={generalEnquiry === "General Enquiry"}
+                onChange={(e) => setGeneralEnquiry(e.target.value)}
+              />
               <label htmlFor="generalEnquiry">General Enquiry</label>
             </li>
-            <li className="rounded">
-              <input type="radio" id="supportRequest" name="queryType" />
+            <li className="rounded_border">
+              <input
+                type="radio"
+                id="supportRequest"
+                name="queryType"
+                value="Support Request"
+                checked={generalEnquiry === "Support Request"}
+                onChange={(e) => setGeneralEnquiry(e.target.value)}
+              />
               <label htmlFor="supportRequest">Support Request</label>
             </li>
           </ul>
@@ -66,9 +94,11 @@ function App() {
               Message<sup>*</sup>
             </label>
             <textarea
-              className="rounded"
+              className="rounded_border"
               id="message"
               name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
           <div className="checkbox_container">
@@ -77,6 +107,11 @@ function App() {
               type="checkbox"
               id="consent"
               name="consent"
+              onChange={(e) => {
+                console.log(e.target.checked);
+                setConsent(e.target.checked);
+              }}
+              checked={consent}
             />
             <label htmlFor="consent">
               I consent to being contacted by the team<sup>*</sup>
