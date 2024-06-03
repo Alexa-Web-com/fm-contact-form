86e1d6e896dc5b562616e213f025e4c2a54c548f
Aleksandra Wilczynska <aleksandra.wilczynska@publicisgroupe.com>
2024-06-03 09:58:18 +0200

    refactor: styles

--- src/components/FormFieldWrapper.tsx
+++ src/components/FormFieldWrapper.tsx
@@ -7,8 +7,8 @@ const FormFieldWrapper = ({ children, validMsg }: IFormFieldWrapper) => {
   const borderClass = validMsg ? "border_error" : "border";
 
   return (
-    <div className="form_field_container">
-      <div className={borderClass}>{children}</div>
+    <div className={borderClass}>
+      {children}
       <p className="error_message">{validMsg}</p>
     </div>
   );
